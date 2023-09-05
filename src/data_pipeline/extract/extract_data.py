5facf186377a3347cef349bd6b64e5fdc521b59f
G, Abhishek <abhishek.g@lowes.com>
2023-09-05 19:20:20 +0530

    structure

--- src/data_pipeline/extract/extract_data.py
+++ src/data_pipeline/extract/extract_data.py
@@ -5,10 +5,3 @@ from src.data_pipeline.transform.transform_data import transform_data
 def extract_data(spark, input_path):
     df = spark.read.csv(input_path, header=True)
     return df
-
-
-# spark = create_spark_session()
-# df = extract_data(spark=spark,
-#                   input_path=r"C:\Users\4906031\PycharmProjects\data_engineering_project\data\input\input.csv")
-# df = transform_data(df)
-# df.show()
--- src/data_pipeline/transform/transform_data.py
+++ src/data_pipeline/transform/transform_data.py
@@ -2,6 +2,5 @@ from pyspark.sql.functions import col, upper
 
 
 def transform_data(df):
-    # transform_df = df.withColumn("country_uppercase", col["Country"].cast("string").upper())
     transform_df = df.withColumn("country_uppercase", upper(col("country")))
     return transform_df
